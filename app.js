777c4e3257a060a4c2c47f6ef7e2939ff61582b8
SarvY1999 <sarveshy193@gmail.com>
2023-04-23 21:26:03 +0530

    changes to server url

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_LOCAL_URI);
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
